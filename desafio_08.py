70f76ca8fb3e495d219f5f557ac577a2d493bdff
Alberth Santana <alberthcosta89@gmail.com>
2026-04-23 19:07:29 -0300

    estudos python

--- desafio_08.py
+++ desafio_08.py
@@ -2,4 +2,4 @@ metros = float(input('Digite a quantidade de metros: '))
 cm = metros * 100
 mm = metros * 1000
 
-print(f'Metros: {metros}, cm: {cm}, mm: {mm}')
+print(f'Metros: {metros}, cm: {cm:.0f}, mm: {mm:.0f}')
